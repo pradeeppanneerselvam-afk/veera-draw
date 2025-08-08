1bb8e9c534ba99b7842c6234b0ad7b5ae33a1188
Pradeep Panneerselvam <pradeep.panneerselvam@gmail.com>
2025-08-08 08:12:56 +0530

    Initial commit

--- firebase-init.js
+++ firebase-init.js
@@ -1,18 +1,13 @@
 // firebase-init.js
-
-// Your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtSOwCDWOv2EmZfM4Nv5B_4eCayh3y8os",
-  authDomain: "coolie-e15ef.firebaseapp.com",
-  projectId: "coolie-e15ef",
-  storageBucket: "coolie-e15ef.firebasestorage.app",
-  messagingSenderId: "133487554623",
-  appId: "1:133487554623:web:37bdd703305165e76a488a",
-  measurementId: "G-QPWPFB60K2"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
-// Initialize Firebase (SDK must be loaded first)
-if (typeof firebase !== 'undefined' && !firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-const db = (typeof firebase !== 'undefined') ? firebase.firestore() : null;
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
