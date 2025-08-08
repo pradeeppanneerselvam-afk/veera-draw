5169c83fc1fa96490a5679cb821befc3d3f44377
Pradeep Panneerselvam <pradeep.panneerselvam@gmail.com>
2025-08-08 08:33:41 +0530

    Initial commit

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,12 @@
-// firebase-init.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyDtSOwCDWOv2EmZfM4Nv5B_4eCayh3y8os",
+  authDomain: "coolie-e15ef.firebaseapp.com",
+  projectId: "coolie-e15ef",
+  storageBucket: "coolie-e15ef.firebasestorage.app",
+  messagingSenderId: "133487554623",
+  appId: "1:133487554623:web:37bdd703305165e76a488a",
+  measurementId: "G-QPWPFB60K2"
 };
 
 firebase.initializeApp(firebaseConfig);
