79ce756aa062fbc15354b680bb2c70c582855026
Jayakrishna <jayakrishna.s@openturf.in>
2025-08-10 21:45:29 +0530

    issue fixed

--- firebase-init.js
+++ firebase-init.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDtSOwCDWOv2EmZfM4Nv5B_4eCayh3y8os",
@@ -6,9 +9,10 @@ const firebaseConfig = {
   storageBucket: "coolie-e15ef.firebasestorage.app",
   messagingSenderId: "133487554623",
   appId: "1:133487554623:web:37bdd703305165e76a488a",
-  measurementId: "G-QPWPFB60K2"
+  measurementId: "G-QPWPFB60K2",
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 window.db = db;
+export { db };
